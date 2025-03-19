94ea444fc85c5d8c580a216819f4dbddd3672708
Miguel Garcia <miguel_tec_programador@outlook.com>
2025-03-19 10:29:46 -0600

    Avance de Solicitud de Fabricacion ya muestra el Almacen en SAE

--- ElastoSystem/Almacen_Fabricacion.Designer.cs
+++ ElastoSystem/Almacen_Fabricacion.Designer.cs
@@ -28,7 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             label1 = new Label();
+            pnlAlmacen = new Panel();
+            btnCerrar = new Button();
+            dgvProductosSAE = new DataGridView();
+            label9 = new Label();
+            btnAbrir = new Button();
+            pnlAlmacen.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProductosSAE).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -43,16 +52,106 @@
             label1.TabIndex = 1;
             label1.Text = "SOLICITUD DE FABRICACION";
             // 
+            // pnlAlmacen
+            // 
+            pnlAlmacen.BackColor = Color.FromArgb(3, 42, 76);
+            pnlAlmacen.Controls.Add(btnCerrar);
+            pnlAlmacen.Controls.Add(dgvProductosSAE);
+            pnlAlmacen.Controls.Add(label9);
+            pnlAlmacen.Location = new Point(36, 96);
+            pnlAlmacen.Name = "pnlAlmacen";
+            pnlAlmacen.Size = new Size(1277, 705);
+            pnlAlmacen.TabIndex = 19;
+            pnlAlmacen.Visible = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.BackColor = Color.FromArgb(255, 102, 0);
+            btnCerrar.Cursor = Cursors.Hand;
+            btnCerrar.FlatAppearance.BorderSize = 0;
+            btnCerrar.FlatStyle = FlatStyle.Flat;
+            btnCerrar.Font = new Font("Montserrat", 11F, FontStyle.Bold);
+            btnCerrar.ForeColor = Color.White;
+            btnCerrar.Location = new Point(1090, 18);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(151, 35);
+            btnCerrar.TabIndex = 19;
+            btnCerrar.Text = "CERRAR";
+            btnCerrar.UseVisualStyleBackColor = false;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // dgvProductosSAE
+            // 
+            dgvProductosSAE.AllowUserToAddRows = false;
+            dgvProductosSAE.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvProductosSAE.BackgroundColor = Color.White;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.White;
+            dataGridViewCellStyle1.Font = new Font("Montserrat", 12F, FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(255, 102, 0);
+            dataGridViewCellStyle1.SelectionForeColor = Color.White;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvProductosSAE.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvProductosSAE.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvProductosSAE.Location = new Point(19, 59);
+            dgvProductosSAE.Name = "dgvProductosSAE";
+            dgvProductosSAE.RowHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = Color.White;
+            dataGridViewCellStyle2.Font = new Font("Montserrat", 12F);
+            dataGridViewCellStyle2.ForeColor = Color.Black;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(255, 102, 0);
+            dataGridViewCellStyle2.SelectionForeColor = Color.White;
+            dgvProductosSAE.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            dgvProductosSAE.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvProductosSAE.Size = new Size(1222, 624);
+            dgvProductosSAE.TabIndex = 18;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Montserrat", 16F, FontStyle.Bold);
+            label9.ForeColor = Color.White;
+            label9.Location = new Point(19, 17);
+            label9.Name = "label9";
+            label9.Size = new Size(263, 30);
+            label9.TabIndex = 17;
+            label9.Text = "ALMACEN ASPEL SAE";
+            // 
+            // btnAbrir
+            // 
+            btnAbrir.BackColor = Color.FromArgb(255, 102, 0);
+            btnAbrir.Cursor = Cursors.Hand;
+            btnAbrir.FlatAppearance.BorderSize = 0;
+            btnAbrir.FlatStyle = FlatStyle.Flat;
+            btnAbrir.Font = new Font("Montserrat", 11F, FontStyle.Bold);
+            btnAbrir.ForeColor = Color.White;
+            btnAbrir.Location = new Point(1034, 114);
+            btnAbrir.Name = "btnAbrir";
+            btnAbrir.Size = new Size(225, 35);
+            btnAbrir.TabIndex = 20;
+            btnAbrir.Text = "ABRIR ALMACEN SAE";
+            btnAbrir.UseVisualStyleBackColor = false;
+            btnAbrir.Click += btnAbrir_Click;
+            // 
             // Almacen_Fabricacion
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.fondocontrolalmacen;
             ClientSize = new Size(1338, 831);
+            Controls.Add(pnlAlmacen);
+            Controls.Add(btnAbrir);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Almacen_Fabricacion";
             Text = "Almacen_Fabricacion";
+            Load += Almacen_Fabricacion_Load;
+            pnlAlmacen.ResumeLayout(false);
+            pnlAlmacen.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvProductosSAE).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -60,5 +159,10 @@
         #endregion
 
         private Label label1;
+        private Panel pnlAlmacen;
+        private DataGridView dgvProductosSAE;
+        private Label label9;
+        private Button btnCerrar;
+        private Button btnAbrir;
     }
 }
--- ElastoSystem/Almacen_Fabricacion.cs
+++ ElastoSystem/Almacen_Fabricacion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +11,82 @@ using System.Windows.Forms;
 
 namespace ElastoSystem
 {
-    public partial class Almacen_Fabricacion: Form
+    public partial class Almacen_Fabricacion : Form
     {
         public Almacen_Fabricacion()
         {
             InitializeComponent();
+        }
+
+        private void Almacen_Fabricacion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MandarALlamarProductos()
+        {
+            try
+            {
+                string update = @"
+                    UPDATE elastosystem_sae_productos
+                    SET Estatus = CASE
+                        WHEN `1M` IS NULL OR `1M` = 0 THEN '--------------'
+                        WHEN Existencia < `TM` THEN 'Critico'
+                        WHEN Existencia >= `TM` AND Existencia < `1M` THEN 'Resurtir'
+                        WHEN Existencia >= `1M` AND Existencia < `3M` THEN 'Programar'
+                        WHEN Existencia >= `3M` AND Existencia < `4M` THEN 'Suficiente'
+                        WHEN Existencia > `4M` THEN 'Sobre Inventario'
+                        ELSE ''
+                    END,
+                    Meses = CASE
+                        WHEN `1M` IS NOT NULL AND `1M` > 0 THEN ROUND(Existencia / `1M`, 2)
+                        ELSE 0
+                    END";
+
+                MySqlConnection conn = new MySqlConnection(VariablesGlobales.ConexionBDElastotecnica);
+                MySqlCommand cmd = new MySqlCommand(update, conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                string query = @"
+                    SELECT *
+                    FROM elastosystem_sae_productos
+                    WHERE Producto NOT LIKE '0%' AND Producto NOT LIKE ' %'
+                    ORDER BY
+                        CASE
+                            WHEN Estatus = 'Critico' THEN 1
+                            WHEN Estatus = 'Resurtir' THEN 2
+                            WHEN Estatus = 'Programar' THEN 3
+                            WHEN Estatus = 'Suficiente' THEN 4
+                            WHEN Estatus = 'Sobre Inventario' THEN 5
+                            ELSE 6
+                        END, Meses ASC";
+
+                MySqlDataAdapter adaptadopr = new MySqlDataAdapter(query, VariablesGlobales.ConexionBDElastotecnica);
+                DataTable dt = new DataTable();
+                adaptadopr.Fill(dt);
+                dgvProductosSAE.DataSource = dt;
+
+                dgvProductosSAE.Columns["ID"].Visible = false;
+                dgvProductosSAE.Columns["TM"].Visible = false;
+                dgvProductosSAE.Columns["CantidadMaxOC"].Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR al llamar productos de Aspel SAE" + ex.Message);
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            pnlAlmacen.Visible = false;
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            pnlAlmacen.Visible = true;
+            MandarALlamarProductos();
         }
     }
 }
